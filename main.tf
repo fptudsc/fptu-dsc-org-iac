4582e7b0cc14b1a8c918f44a663566081013f299
baolongv3 <longnbnguyen@kms-technology.com>
2023-05-21 23:53:47 +0700

    add(main): Add admin user

--- main.tf
+++ main.tf
@@ -6,10 +6,17 @@ resource "github_team" "teams" {
 }
 
 resource "github_membership" "user_membership" {
-  for_each = local.full_branch_email_list
+  for_each = local.user_list
   username = each.value
   role     = "member"
 }
+
+resource "github_membership" "admin_membership" {
+  for_each = local.admin_list
+  username = each.value
+  role     = "admin"
+}
+
 
 resource "github_team_members" "some_team_members" {
   depends_on = [github_membership.user_membership]

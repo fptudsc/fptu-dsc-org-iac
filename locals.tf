cc6fa8ce59ecfb0e465a2abc377bad55234a40e6
baolongv3 <longnbnguyen@kms-technology.com>
2023-05-21 21:43:43 +0700

    add(var): Add private_key

--- locals.tf
+++ locals.tf
@@ -1,5 +1,4 @@
 locals {
-    branches_data = yamldecode(file("${path.module}/config.yaml"))
-    full_branch_email_list = toset(flatten([for k in local.branches_data : k.member_emails]))
-    pem_file = file("${path.module}/terraform-key.pem")
+  branches_data          = yamldecode(file("${path.module}/config.yaml"))
+  full_branch_email_list = toset(flatten([for k in local.branches_data : k.member_emails]))
 }
--- provider.tf
+++ provider.tf
@@ -9,11 +9,11 @@ terraform {
 
 # Configure the GitHub Provider
 provider "github" {
-    owner = "fptudsc"
-    app_auth  {
-        id = var.app_id
-        installation_id = var.installation_id
-        pem_file = local.pem_file
-    }
+  owner = "fptudsc"
+  app_auth {
+    id              = var.app_id
+    installation_id = var.installation_id
+    pem_file        = var.private_key
+  }
 
 }

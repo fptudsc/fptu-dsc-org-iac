db1b5e17d11ccc69fe3b0425100a7d27d43ec557
baolongv3 <longnbnguyen@kms-technology.com>
2023-05-27 21:03:29 +0700

    change(locals): fix local

--- locals.tf
+++ locals.tf
@@ -1,5 +1,5 @@
 locals {
   branches_data = yamldecode(file("${path.module}/config.yaml"))
   admin_list    = toset(flatten([for k in local.branches_data : k.member_username if k.admin]))
-  user_list      = toset([for user in flatten([for k in local.branches_data : k.member_username if !k.admin]) : user if !contains(local.admin_list, user)])
+  user_list     = toset([for user in flatten([for k in local.branches_data : k.member_username if !k.admin]) : user if !contains(local.admin_list, user)])
 }
